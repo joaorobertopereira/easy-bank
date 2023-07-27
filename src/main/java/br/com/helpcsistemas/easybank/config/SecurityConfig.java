bc3b140120368201953ed001a5dfd0016c1e90d0
João Roberto de França Pereira <joaorobertof@gmail.com>
2023-07-27 16:25:32 -0300

    update project

--- src/main/java/br/com/helpcsistemas/easybank/config/SecurityConfig.java
+++ src/main/java/br/com/helpcsistemas/easybank/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                         config.setMaxAge(3600L);
                         return config;
                     }
-                })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact","/register")
+                })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/contact","/register")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests)->requests
